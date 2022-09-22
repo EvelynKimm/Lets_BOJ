fcef79404cbf5cb0a8731494444f9b5a9d2074a2
EvelynKimm <108613992+EvelynKimm@users.noreply.github.com>
2022-09-22 21:02:58 +0900

    [Silver III] Title: 수강신청, Time: 1364 ms, Memory: 69780 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.java
+++ 백준/Silver/13414. 수강신청/수강신청.java
@@ -9,11 +9,13 @@ class Main {
         int K = Integer.parseInt(st.nextToken());
         int L = Integer.parseInt(st.nextToken());
         LinkedHashSet <String> set = new LinkedHashSet<>();
+        //HashSet처럼 중복은 허락하지 않으나, 입력한 순서대로 출력가능
+        //HashSet은 순서 허용 X
 
         for (int i=0; i<L; i++) {
             String n = br.readLine();
             //String으로 받아서 contains메서드로 첫번째 중복 없애준다
-            
+
             if(set.contains(n)) set.remove(n);
             set.add(n);
 
