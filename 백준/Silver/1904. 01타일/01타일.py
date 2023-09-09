e755ef37b837bcc4f119caea892296493ae7799b
EvelynKimm <108613992+EvelynKimm@users.noreply.github.com>
2023-09-09 21:47:48 +0900

    [Silver III] Title: 01타일, Time: 356 ms, Memory: 69788 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,14 @@
+#1904
+#점화식을 통해 풀이 가능
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] * 1000001
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3, n+1):
+    dp[i] = (dp[i-1] + dp[i-2]) % 15746
+
+print(dp[n])
